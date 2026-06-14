4e9cb0fbe407bf313c7ec48b0a9f7239aa394ca1
Sneha-choudhary-1 <choudhary.20p8@gmail.com>
2026-06-14 10:59:20 +0530

    Initial commit - Python GUI Calculator
    Added a README section outlining features and technology used.

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# Python GUI Calculator
+A simple calculator made using python Tkinter.
+
+## Features
+  - Addition
+  - Subtraction
+  - Multiplication
+  - Division
+  - Backspace
+  - Percentage
+  - Square Root
+  - History Panel
+  - Dark theme
+  ## Technology
+  - Python
+  - Tkinter
